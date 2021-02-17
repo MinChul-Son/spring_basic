753c48732e45fc2983c61af7681af83dcbac2fb2
MinChul-Son <smc5236@naver.com>
2021-02-17 22:10:22 +0900

    21/2/17 spring_study3

--- hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
+++ hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -1,10 +1,12 @@
 package hello.hellospring;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloSpringApplicationTests {
+
 
 	@Test
 	void contextLoads() {
